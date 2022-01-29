f7d81235b10c49b535a4533935ca6da27f2262a7
Daniel-EST <dd_santos@id.uff.br>
2022-01-29 01:28:15 -0300

    Initial commit

---
+++ server/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=W0613, C0116
+# type: ignore[union-attr]
+
+import logging
+
+from flask import Flask
+from flask import render_template, jsonify, make_response
+
+from utils.translators.portuguese import num2ptbr
+from utils.translators.chinese import num2chinese, chinese2pinyin
+
+
+logging.basicConfig(level=logging.INFO)
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/convert/<int:number>")
+def converter(number):
+    ptbr = num2ptbr(number)
+    cn = num2chinese(number)
+    pinyin = chinese2pinyin(cn)
+    results = {
+        'num': number,
+        'ptbr': ptbr,
+        'cn': cn,
+        'pinyin': pinyin
+    }
+    res = make_response(jsonify(results), 200)
+    logging.info(res)
+    return res
+
+if __name__ == "__main__":
+    logging.info("Server started")
+    app.run(threaded=True, port=5000)
---
+++ server/utils/translators/portuguese.py
@@ -0,0 +1,3 @@
+from num2words import num2words
+
+num2ptbr = lambda num: num2words(num, lang='pt_BR')
